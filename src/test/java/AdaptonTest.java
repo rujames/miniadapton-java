548f568e0c77a6454b32556a8f14188d09fca8da
Ru James <ruairidh.d.james@gmail.com>
2022-06-12 13:30:58 +0100

    Add Fibonacci test case

--- src/test/java/AdaptonTest.java
+++ src/test/java/AdaptonTest.java
@@ -1,6 +1,10 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 class AdaptonTest {
 
     @Test
@@ -55,6 +59,49 @@ class AdaptonTest {
 
             r1.set(10);
             assertEquals(24, a.force());
+        }
+    }
+
+    @Test
+    void fibonacci() {
+        { // values are computed from the input refs
+            final DirectedComputationGraph dcg = new DirectedComputationGraph();
+            final Adapton.Ref<Integer> f0 = dcg.ref(null);
+            final Adapton.Ref<Integer> f1 = dcg.ref(null);
+
+            final List<Adapton<Integer>> fibs = new ArrayList<>();
+            fibs.add(f0);
+            fibs.add(f1);
+            IntStream.range(2, 10).forEach((i) ->
+                    fibs.add(dcg.expr(() -> fibs.get(i - 1).force() + fibs.get(i - 2).force()))
+            );
+
+            f0.set(0);
+            f1.set(0);
+            assertEquals(0, fibs.get(9).force());
+
+            f0.set(1);
+            f1.set(1);
+            assertEquals(55, fibs.get(9).force());
+        }
+        { // evaluations are memoized
+            final DirectedComputationGraph dcg = new DirectedComputationGraph();
+            final Adapton.Ref<Integer> f0 = dcg.ref(1);
+            final Adapton.Ref<Integer> f1 = dcg.ref(1);
+
+            final List<Integer> evaluations = new ArrayList<>();
+            final List<Adapton<Integer>> fibs = new ArrayList<>();
+            fibs.add(f0);
+            fibs.add(f1);
+            IntStream.range(2, 10).forEach((i) ->
+                fibs.add(dcg.expr(() -> {
+                    evaluations.add(i);
+                    return fibs.get(i - 1).force() + fibs.get(i - 2).force();
+                }))
+            );
+
+            assertEquals(55, fibs.get(9).force());
+            assertEquals(8, evaluations.size());
         }
     }
 
